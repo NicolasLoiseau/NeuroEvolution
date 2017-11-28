f56dc1cc562ec34c2c58753fbb87ffe6ac5bd3d8
Nicolas <nicolasloiseau@hotmail.fr>
2017-11-28 23:38:20 +0100

    export and save individual in file

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -21,3 +21,11 @@ class NeuralNetwork(object):
     def mutate(self):
         self.syn0 += (2 * np.random.random((self.input_nb, self.output_nb)) - 1) / 10
         self.syn1 += (2 * np.random.random((self.output_nb, self.output_nb)) - 1) / 10
+
+    def export(self):
+        return {
+            'column_nb': self.column_nb,
+            'row_nb': self.row_nb,
+            'syn0': self.syn0.tolist(),
+            'syn1': self.syn1.tolist()
+        }
