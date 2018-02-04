7ad127623160adafc53192ed578b623777e2be92
Julien Perrin <1julien.perrin@gmail.com>
2018-02-04 18:35:21 +0100

    gpu functions

---
+++ gravity.py
@@ -0,0 +1,109 @@
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
+from pycuda import characterize
+import numpy as np
+
+a = np.zeros((6, 3)).astype(int)
+
+
+b = a.copy()
+b[0, :] = 1
+
+c = np.ones((6, 3)).astype(int)
+c[1, :] = 1
+
+d = a.copy()
+d[0:4, :] = 1
+
+e = a.copy()
+e[1, [0, 2]] = 1
+e[2, 1] = 1
+e[0, 1] = 1
+
+f = np.asarray((a, b, c, d, e))
+
+
+print("-" * 80)
+print(f)
+
+gravity_kernel_template = """
+       __global__ void gravity(float *a)
+       {  
+           int idb = blockIdx.x * %(l)s * blockDim.y;
+
+           int ind = 0;
+
+               while (ind < %(l)s)
+               { 
+                   if (a[idb + ((%(l)s - 1) - (ind))* %(c)s + threadIdx.y] == 0)
+                   {
+                       ind++;
+                   }
+
+                   else
+                   {
+                       if (ind == 0)
+                       {
+                           ind++;
+                       }
+
+                       else
+                       {  
+                          if (a[idb + ((%(l)s - 1) - (ind)+1)* %(c)s + threadIdx.y] != 0)
+                          {
+                               ind++;
+                          }
+                          else
+                          {
+                              int temp = a[idb + ((%(l)s - 1) - (ind))* %(c)s + threadIdx.y];
+
+                              a[idb + ((%(l)s - 1) - (ind)+1)* %(c)s + threadIdx.y] = temp;
+
+                              a[idb + ((%(l)s - 1) - (ind))* %(c)s + threadIdx.y] = 0;
+
+                              ind = 0;
+                       }
+                       }
+                   }
+               }
+           }   
+       }
+       """
+
+def gravity_gpu(f):
+
+    # Constants
+    g = f.shape[0]
+    l = f.shape[1]
+    c = f.shape[2]
+
+    # Define type
+    f = f.astype(np.float32)
+
+    # Create memory space on device
+    f_gpu = cuda.mem_alloc(f.nbytes)
+
+    # copy items on device
+    cuda.memcpy_htod(f_gpu, f)
+
+    # Define cuda kernel
+    gravity_kernel = gravity_kernel_template % {'l': l, 'c': c}
+    mod = SourceModule(gravity_kernel)
+
+    # Define function form kernel (on gpu)
+    func = mod.get_function("gravity")
+
+    # Apply function (on gpu)
+    func(f_gpu, grid=(g, 1), block=(1, c, 1))
+
+    # Empty array to store result
+    f_new = np.empty_like(f)
+
+    # copy results back from device to host
+    cuda.memcpy_dtoh(f_new, f_gpu)
+
+    return f_new
+
+f_test = gravity_gpu(f)
+
+print(f_test)
---
+++ remodelling.py
@@ -0,0 +1,110 @@
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+from pycuda import characterize
+import numpy as np
+import time
+
+# Remodelling
+
+generation_size = 2
+
+a = np.zeros((generation_size, 6, 3)).astype(int)
+a[:, :, 0] = 2
+a[0, 1, 2] = 1
+scores = np.array([2,3]).astype(int)
+
+spts = np.zeros(generation_size).astype(int)
+
+epts = np.array([5,3]).astype(int)
+
+play_vect = np.ones(generation_size)
+
+print(a)
+print(scores)
+print(spts)
+print(epts)
+
+# Cuda kernel
+remodelling_kernel_template = """
+    __global__ void Remodelling(float *a, int *spts, int *epts, float *scores, float *caps, int * vect_play)
+    {
+        if (play_vect[threadIdx.x] == 1)
+        {
+            int temp = 0;
+
+            int ids = threadIdx.x * %(l)s * %(c)s +  spts[threadIdx.x];
+
+            int ide = threadIdx.x * %(l)s * %(c)s +  epts[threadIdx.x];
+
+            temp[threadIdx.x] = a[ids];
+
+            a[ids] = 0;
+
+            if(a[ide] != temp)
+            {
+                a[ide] += temp;
+
+                scores[threadIdx.x] += a[ide];
+            }
+            else
+            {
+                scores[threadIdx.x] += 2 * temp;
+            }
+            caps[threadIdx.x] = %(cap0)s + scores[threadIdx.x]/100
+        }
+    }
+    """
+
+def remodelling_gpu(a, spts, epts, scores, caps, play_vect, cap0=5):
+
+    # Constants
+    g = a.shape[0]
+    l = a.shape[1]
+    c = a.shape[2]
+
+    # Define type for gpu computation
+    a = a.astype(np.float32)
+    scores = scores.astype(np.float32)
+    spts = spts.astype(np.int32)
+    epts = epts.astype(np.int32)
+    play_vect = play_vect.astype(np.int32)
+    caps = caps.astype(np.float32)
+
+    # Create memory space on device
+    a_gpu = cuda.mem_alloc(a.nbytes)
+    scores_gpu = cuda.mem_alloc(scores.nbytes)
+    spts_gpu = cuda.mem_alloc(spts.nbytes)
+    epts_gpu = cuda.mem_alloc(epts.nbytes)
+    play_vect_gpu = cuda.mem_alloc(play_vect.nbytes)
+    caps_gpu = cuda.mem_alloc(play_vect.nbytes)
+
+    # Copy items on device
+    cuda.memcpy_htod(a_gpu, a)
+    cuda.memcpy_htod(scores_gpu, scores)
+    cuda.memcpy_htod(spts_gpu, spts)
+    cuda.memcpy_htod(epts_gpu, epts)
+    cuda.memcpy_htod(play_vect_gpu, play_vect)
+    cuda.memcpy_htod(caps_gpu, caps)
+
+    # Empty arrays to store results
+    a_new = np.empty_like(a)
+    scores_new = np.empty_like(scores)
+    caps_new = np.empty_like(caps)
+
+    # Define remodelling kernel
+    remodelling_kernel = remodelling_kernel_template % {'l': l, 'c': c, 'cap0': cap0}
+    mod = SourceModule(remodelling_kernel)
+
+    # Define function form kernel (on GPU)
+    func = mod.get_function("Remodelling")
+
+    # Apply function (on GPU)
+    func(a_gpu, spts_gpu, epts_gpu, scores_gpu, caps_gpu, play_vect_gpu, grid=(1, 1), block=(g, 1, 1))
+
+    # Copy results back from device
+    cuda.memcpy_dtoh(a_new, a_gpu)
+    cuda.memcpy_dtoh(scores_new, scores_gpu)
+    cuda.memcpy_dtoh(caps_new, scores_new)
+
+    return a_new, scores_new, caps_new
