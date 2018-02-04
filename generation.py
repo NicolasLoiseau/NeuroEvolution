ef9f72de834fd0833c270a438559cfffab071f56
unknown <vnguyenx94@aol.com>
2018-02-04 21:44:01 +0100

    final - remodeling

--- generation.py
+++ generation.py
@@ -22,14 +22,17 @@ class Generation:
         self.cap0 = cap
         self.skeletons = np.zeros((self.generation_size,self.row_nb, self.column_nb)).astype(int)
         self.scores = np.zeros(self.generation_size)
-        self.caps = np.ones(self.generation_size)*self.cap0
         self.move_mapper = self.build_move_mapper()
         self.intelligence = NeuralNetwork(self.generation_size, self.row_nb, self.column_nb)
+        self.moves = np.zeros((self.generation_size,2,2))
+
+    @property
+    def caps(self):
+        return np.ones(self.generation_size)*self.cap0 + self.scores // 100
 
     def reset(self):
         self.skeletons = np.zeros((self.generation_size,self.row_nb, self.column_nb)).astype(int)
         self.scores = np.zeros(self.generation_size)
-        self.caps = np.ones(self.generation_size)*self.cap0
 
     def fill(self):
         height = self.row_nb // 2
@@ -40,9 +43,14 @@ class Generation:
     def game_over(self):
         return self.skeletons.sum(axis=2)[:, 0] != 0
 
-    def remodeling(self, start_point, end_point, play_range):
+    def remodeling(self, play_range):
         for i in play_range:
-            self.individuals[i].remodelling
+            if self.skeletons[i][self.end_pt(i)] != self.skeletons[i][self.start_pt(i)]:
+                self.scores[i] += (self.skeletons[i][self.start_pt(i)] + self.skeletons[i][self.end_pt(i)])
+                self.skeletons[i][self.end_pt(i)] += self.skeletons[i][self.start_pt(i)]
+            else:
+                self.scores[i] += (2 * self.skeletons[i][self.start_pt(i)])
+            self.skeletons[i][self.start_pt(i)] = 0
 
     def refill(self, not_equal, play_range):
         for i in play_range:
@@ -58,11 +66,11 @@ class Generation:
 
 
     def one_play(self):
-        start_pt, end_pt = self.get_move()
-        not_equal = [self.skeletons[i][start_pt[i][0]][start_pt[i][1]] != self.skeletons[i][end_pt[i][0]][end_pt[i][1]] for i in range(self.generation_size)]
+        self.moves = self.get_move()
+        not_equal = [self.skeletons[i][self.start_pt(i)] != self.skeletons[i][self.end_pt(i)] for i in range(self.generation_size)]
         play_range = np.where(np.invert(self.game_over))[0]
         play_vec = self.game_over.astype(int)
-        self.remodeling(start_pt, end_pt, play_vec)
+        self.remodeling(play_range)
         self.refill(not_equal, play_range)
         self.gravity()
 
@@ -130,13 +138,19 @@ class Generation:
         nn_input = self.skeletons.reshape((self.generation_size, 1, self.row_nb * self.column_nb))
         return self.intelligence(nn_input)
 
+    def start_pt(self, i):
+        return self.moves[i, 0, 0], self.moves[i, 0, 1]
+
+    def end_pt(self, i):
+        return self.moves[i, 1, 0], self.moves[i, 1, 1]
+
 if __name__ == '__main__':
-    gen = Generation(4, 6, 3, 7)
+    gen = Generation(1, 6, 3, 7)
     #print([i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i])
     gen.fill()
     #gravity_gpu(gen.skeletons)
-    moves = gen.get_move()
-    print("ok")
+    gen.play()
+
 
 
 
