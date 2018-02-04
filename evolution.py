b7bbef1c60569218382a368f7df187653df81723
Nicolas <nicolasloiseau@hotmail.fr>
2018-02-04 16:20:34 +0100

    switch btw cpu and gpu in evolution

--- evolution.py
+++ evolution.py
@@ -10,31 +10,45 @@ from generation import Generation
 
 
 class Evolution:
-    def __init__(self, generation_nb, generation_size, game_per_generation, row_nb, column_nb, cap):
+    def __init__(self, generation_nb, generation_size, game_per_generation, row_nb, column_nb, cap, use_gpu=False):
         self.generation_nb = generation_nb
         self.generation_size = generation_size
         self.game_per_generation = game_per_generation
         self.row_nb = row_nb
         self.column_nb = column_nb
         self.cap = cap
-        self.generation = Generation(generation_size, row_nb, column_nb, cap)
+        self.use_gpu = use_gpu
+        if self.use_gpu:
+            self.generation = Generation(generation_size, row_nb, column_nb, cap)
+        else:
+            self.generation = [Individual(row_nb, column_nb, cap) for _ in range(0, generation_size)]
         self.score_mean = list()
         self.score_max = list()
         self.score_min = list()
 
     def nextgen(self):
         scores = np.zeros(self.generation_size)
-        for i in range(self.game_per_generation):
-            scores += np.array(list(self.generation.play()))
+        if self.use_gpu:
+            for i in range(self.game_per_generation):
+                scores += np.array(list(self.generation.play()))
+        else:
+            for i in range(self.game_per_generation):
+                scores += np.array(list(map(lambda ind: ind.play(), self.generation)))
         scores /= self.game_per_generation
         self.score_mean.append(np.mean(scores))
         self.score_max.append(np.max(scores))
         self.score_min.append(np.min(scores))
         median = np.median(scores)
-        newgen = [gen for score, gen in zip(scores, self.generation.individuals) if score >= median]
+        if self.use_gpu:
+            newgen = [gen for score, gen in zip(scores, self.generation.individuals) if score >= median]
+        else:
+            newgen = [gen for score, gen in zip(scores, self.generation) if score >= median]
         for k in range(self.generation_size - len(newgen)):
             newgen.append(newgen[k].mutate())
-        self.generation.individuals = newgen
+        if self.use_gpu:
+            self.generation.individuals = newgen
+        else:
+            self.generation = newgen
 
     def train(self):
         for i in range(self.generation_nb):
