3d723cf4ed4a4e0e1b224e6b643dc823f1e5259f
Nicolas <nicolasloiseau@hotmail.fr>
2018-02-04 17:30:21 +0100

    correct nn

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -6,7 +6,7 @@ import pycuda.autoinit
 
 
 kernel_code_template = """
-_global_ void MatrixMulKernel(float *a, float *b, float *c)
+__global__ void MatrixMulKernel(float *a, float *b, float *c)
 {
     float Pvalue = 0;
 
@@ -34,28 +34,21 @@ class NeuralNetwork(object):
         self.syn2 = syn2 or self.syn_init(self.output_nb, self.output_nb)
 
     def syn_init(self, x, y):
-        return np.array([2 * np.random.random((x, y)) - 1 for _ in range(self.generation_size)])
+        return np.array([2 * np.random.random((x, y)) - 1 for _ in range(self.generation_size)]).astype(np.float32)
 
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
-    @staticmethod
-    def flat(array):
-        return array.reshape(array.shape[1]*array.shape[0], array.shape[2])
-
-    def unflat(self, array):
-        return array.reshape(self.generation_size, array.shape[0] // self.generation_size, array.shape[1])
-
     def __call__(self, input):
-        l1 = self.sigmoid(self.gpu_mult(self.flat(input), self.flat(self.syn0)))
-        l2 = self.sigmoid(self.gpu_mult(l1, self.flat(self.syn1)))
-        return self.sigmoid(self.gpu_mult(l2, self.flat(self.syn2)))
+        l1 = self.sigmoid(self.gpu_mult(input, self.syn0))
+        l2 = self.sigmoid(self.gpu_mult(l1, self.syn1))
+        return self.sigmoid(self.gpu_mult(l2, self.syn2))
 
     def mutate(self, index):
-        self.syn0[index] += (2 * np.random.random((self.input_nb, self.output_nb)) - 1) / 5
-        self.syn1[index] += (2 * np.random.random((self.output_nb, self.output_nb)) - 1) / 5
-        self.syn2[index] += (2 * np.random.random((self.output_nb, self.output_nb)) - 1) / 5
+        self.syn0[index] += (2 * np.random.random((self.input_nb, self.output_nb)).astype(np.float32) - 1) / 5
+        self.syn1[index] += (2 * np.random.random((self.output_nb, self.output_nb)).astype(np.float32) - 1) / 5
+        self.syn2[index] += (2 * np.random.random((self.output_nb, self.output_nb)).astype(np.float32) - 1) / 5
 
     def export(self):
         return {
@@ -68,13 +61,13 @@ class NeuralNetwork(object):
 
     def gpu_mult(self, a_cpu, b_cpu):
 
-        n = a_cpu.shape[0] // self.generation_size
-        p = a_cpu.shape[1]
-        l = b_cpu.shape[1]
+        n = a_cpu.shape[1]
+        p = a_cpu.shape[2]
+        l = b_cpu.shape[2]
 
         # transfer host (CPU) memory to device (GPU) memory
-        a_gpu = gpuarray.to_gpu(a_cpu)
-        b_gpu = gpuarray.to_gpu(b_cpu)
+        a_gpu = gpuarray.to_gpu(a_cpu.astype(np.float32))
+        b_gpu = gpuarray.to_gpu(b_cpu.astype(np.float32))
 
         # create empty gpu array for the result (C = A * B)
         c_gpu = gpuarray.empty((n * self.generation_size, l), np.float32)
